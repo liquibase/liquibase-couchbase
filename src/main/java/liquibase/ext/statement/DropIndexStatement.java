94137ae5590d2ecbab220a6f3de5170e030a0954
konstantin-umanets <konstantin.umanets@weigandt-consulting.com>
2023-02-01 16:59:32 +0400

    Add drop index change

---
+++ src/main/java/liquibase/ext/statement/DropIndexStatement.java
@@ -0,0 +1,38 @@
+package liquibase.ext.statement;
+
+import com.couchbase.client.java.Cluster;
+import com.couchbase.client.java.manager.query.DropQueryIndexOptions;
+import liquibase.ext.database.CouchbaseConnection;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Optional;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@RequiredArgsConstructor
+public class DropIndexStatement extends CouchbaseStatement {
+    private final String indexName;
+    private final String bucketName;
+    private final String collectionName;
+    private final String scopeName;
+
+    @Override
+    public void execute(CouchbaseConnection connection) {
+        Cluster cluster = connection.getCluster();
+        String bucket = Optional.ofNullable(bucketName)
+                .filter(StringUtils::isNotBlank)
+                .orElseGet(() -> connection.getDatabase().name());
+
+        if (StringUtils.isNotBlank(collectionName) || StringUtils.isNotBlank(scopeName)) {
+            DropQueryIndexOptions options = DropQueryIndexOptions.dropQueryIndexOptions()
+                    .collectionName(collectionName)
+                    .scopeName(scopeName);
+            cluster.queryIndexes().dropIndex(bucket, indexName, options);
+        } else {
+            cluster.queryIndexes().dropIndex(bucket, indexName);
+        }
+    }
+}
