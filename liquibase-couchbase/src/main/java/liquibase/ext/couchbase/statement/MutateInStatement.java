2d389cce7af4d63daedf2f80d8f627914b44b00d
Roman Oborin <roman.oborin@weigandt-consulting.com>
2023-02-17 11:36:14 +0000

    Cos 32 implement change mutate

---
+++ liquibase-couchbase/src/main/java/liquibase/ext/couchbase/statement/MutateInStatement.java
@@ -0,0 +1,36 @@
+package liquibase.ext.couchbase.statement;
+
+import com.couchbase.client.java.Collection;
+
+import liquibase.ext.couchbase.database.CouchbaseConnection;
+import liquibase.ext.couchbase.operator.BucketOperator;
+import liquibase.ext.couchbase.operator.ClusterOperator;
+import liquibase.ext.couchbase.types.Keyspace;
+import liquibase.ext.couchbase.types.subdoc.MutateIn;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class MutateInStatement extends CouchbaseStatement {
+
+    private final MutateIn mutate;
+
+    @Override
+    public void execute(ClusterOperator clusterOperator) {
+        Keyspace keyspace = mutate.getKeyspace();
+        BucketOperator bucketOperator = clusterOperator.getBucketOperator(keyspace.getBucket());
+        Collection collection = bucketOperator.getCollection(keyspace.getCollection(), keyspace.getScope());
+
+        collection.mutateIn(mutate.getId(), mutate.getSpecs());
+    }
+
+    @Override
+    public void execute(CouchbaseConnection connection) {
+        execute(new ClusterOperator(connection.getCluster()));
+    }
+
+}
+
