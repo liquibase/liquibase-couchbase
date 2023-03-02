8d4a5f2dd1cec386cadefa1718be7a84f67a3444
zorikzaqaryan <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>
2023-03-02 14:47:50 +0400

    COS-34. Drop scope

---
+++ liquibase-couchbase/src/main/java/liquibase/ext/couchbase/change/DropScopeChange.java
@@ -0,0 +1,45 @@
+package liquibase.ext.couchbase.change;
+
+import liquibase.change.DatabaseChange;
+import liquibase.ext.couchbase.statement.DropScopeStatement;
+import liquibase.servicelocator.PrioritizedService;
+import liquibase.statement.SqlStatement;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Part of change set package. Responsible for drop scope with specified name
+ * @link <a href="https://docs.couchbase.com/server/current/n1ql/n1ql-language-reference/dropscope.html">Reference documentation</a>
+ * @see DropScopeStatement
+ */
+
+@DatabaseChange(
+        name = "dropScope",
+        description = "Drop scope with validation " +
+                "https://docs.couchbase.com/server/current/n1ql/n1ql-language-reference/dropscope.html",
+        priority = PrioritizedService.PRIORITY_DATABASE,
+        appliesTo = {"database"}
+)
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DropScopeChange extends CouchbaseChange {
+    private String bucketName;
+    private String scopeName;
+
+    @Override
+    public String getConfirmationMessage() {
+        return String.format("Scope %s has been successfully dropped", scopeName);
+    }
+
+    @Override
+    public SqlStatement[] generateStatements() {
+        return new SqlStatement[] {
+                new DropScopeStatement(scopeName, bucketName)
+        };
+    }
+
+}
---
+++ liquibase-couchbase/src/test/java/integration/statement/DropScopeStatementTest.java
@@ -0,0 +1,33 @@
+package integration.statement;
+
+import com.couchbase.client.core.error.ScopeNotFoundException;
+import common.RandomizedScopeTestCase;
+import liquibase.ext.couchbase.statement.DropScopeStatement;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+
+class DropScopeStatementTest extends RandomizedScopeTestCase {
+
+    @Test
+    void Should_drop_scope() {
+        DropScopeStatement dropScopeStatement = new DropScopeStatement(scopeName, bucketName);
+
+        dropScopeStatement.execute(clusterOperator);
+
+        assertThat(clusterOperator.getBucketOperator(bucketName).hasScope(scopeName)).isFalse();
+    }
+
+    @Test
+    void Should_throw_scope_not_found() {
+        String scope = "notExistingScope";
+        DropScopeStatement dropScopeStatement = new DropScopeStatement(scope, bucketName);
+
+        assertThatExceptionOfType(ScopeNotFoundException.class)
+                .isThrownBy(() -> dropScopeStatement.execute(clusterOperator))
+                .withMessage("Scope [%s] not found.", scope);
+    }
+
+}
