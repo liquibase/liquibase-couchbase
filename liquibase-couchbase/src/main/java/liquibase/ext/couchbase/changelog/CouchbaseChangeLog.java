2d5b5bb1151908e264a885579ae09e33a2dc3537
DDashko <DDashko@beeline.kz>
2023-03-03 17:31:31 +0600

    COS-114. Changed ChangeLog date type

--- liquibase-couchbase/src/main/java/liquibase/ext/couchbase/changelog/CouchbaseChangeLog.java
+++ liquibase-couchbase/src/main/java/liquibase/ext/couchbase/changelog/CouchbaseChangeLog.java
@@ -1,17 +1,12 @@
 package liquibase.ext.couchbase.changelog;
 
-import com.couchbase.client.core.deps.com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.couchbase.client.core.deps.com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import liquibase.changelog.ChangeSet;
-import liquibase.ext.couchbase.serializer.LocalDateTimeDeserializer;
-import liquibase.ext.couchbase.serializer.LocalDateTimeSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
 import java.util.Set;
 
 @Getter
@@ -24,9 +19,7 @@ public class CouchbaseChangeLog {
     private String id;
     private String author;
     private String checkSum;
-    @JsonSerialize(using = LocalDateTimeSerializer.class)
-    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
-    private LocalDateTime dateExecuted;
+    private String dateExecuted;
     private String tag;
     private ChangeSet.ExecType execType;
     private String description;
