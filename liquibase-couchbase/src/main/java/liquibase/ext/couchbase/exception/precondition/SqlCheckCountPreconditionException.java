b16213c250f0760e1fa60b9b595abd1197906117
Tigran Khojoyan <tigran.khojoyan@weigandt-consulting.com>
2023-04-18 19:03:09 +0400

    COS-214 Added functionality to execute sql++ count query as precondition

---
+++ liquibase-couchbase/src/main/java/liquibase/ext/couchbase/exception/precondition/SqlCheckCountPreconditionException.java
@@ -0,0 +1,26 @@
+package liquibase.ext.couchbase.exception.precondition;
+
+import liquibase.changelog.DatabaseChangeLog;
+import liquibase.exception.PreconditionFailedException;
+import liquibase.precondition.Precondition;
+import lombok.Getter;
+
+import static java.lang.String.format;
+
+/**
+ * An exception thrown when scope does not exist. Thrown by {@link liquibase.ext.couchbase.precondition.SqlCheckCountPrecondition}
+ * @see PreconditionFailedException
+ */
+
+@Getter
+public class SqlCheckCountPreconditionException extends PreconditionFailedException {
+
+    private static final String template = "Sql precondition query[%s] result is different then expected count[%d]";
+    private final String message;
+
+    public SqlCheckCountPreconditionException(String query, Integer count,
+                                              DatabaseChangeLog changeLog, Precondition precondition) {
+        super(format(template, query, count), changeLog, precondition);
+        message = format(template, query, count);
+    }
+}
