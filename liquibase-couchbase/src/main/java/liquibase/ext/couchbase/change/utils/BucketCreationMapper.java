5d5c50e8f0bea4f4bc98a810882d3c80cb0eb06d
Dmitry Shanko <Dmitry.Shanko@tesco.com>
2023-05-15 10:26:42 +0400

    COS-255: refactor to be able to execute changeSets on CE version of couchbase

--- liquibase-couchbase/src/main/java/liquibase/ext/couchbase/change/utils/BucketCreationMapper.java
+++ liquibase-couchbase/src/main/java/liquibase/ext/couchbase/change/utils/BucketCreationMapper.java
@@ -1,5 +1,7 @@
 package liquibase.ext.couchbase.change.utils;
 
+import java.time.Duration;
+
 import com.couchbase.client.java.manager.bucket.BucketSettings;
 import com.couchbase.client.java.manager.bucket.CreateBucketOptions;
 import com.couchbase.client.java.manager.bucket.StorageBackend;
@@ -7,8 +9,6 @@ import liquibase.ext.couchbase.change.CreateBucketChange;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-
-import java.time.Duration;
 
 import static com.couchbase.client.java.manager.bucket.CreateBucketOptions.createBucketOptions;
 
@@ -23,18 +23,25 @@ public class BucketCreationMapper {
     }
 
     public BucketSettings bucketSettings() {
-        return BucketSettings.create(change.getBucketName())
-                .compressionMode(change.getCompressionMode())
+        BucketSettings bucketSettings = BucketSettings.create(change.getBucketName())
                 .flushEnabled(change.getFlushEnabled())
                 .ramQuotaMB(change.getRamQuotaMB())
                 .numReplicas(change.getNumReplicas())
                 .replicaIndexes(change.getReplicaIndexes())
-                .maxExpiry(Duration.ofHours(change.getMaxExpiryInHours()))
                 .bucketType(change.getBucketType())
-                .conflictResolutionType(change.getConflictResolutionType())
                 .evictionPolicy(change.getEvictionPolicy())
                 .minimumDurabilityLevel(change.getMinimumDurabilityLevel())
                 .storageBackend(StorageBackend.of(change.getStorageBackend()));
+        if (change.getCompressionMode() != null) {
+            bucketSettings = bucketSettings.compressionMode(change.getCompressionMode());
+        }
+        if (change.getConflictResolutionType() != null) {
+            bucketSettings = bucketSettings.conflictResolutionType(change.getConflictResolutionType());
+        }
+        if (change.getMaxExpiryInHours() != null) {
+            bucketSettings = bucketSettings.maxExpiry(Duration.ofHours(change.getMaxExpiryInHours()));
+        }
+        return bucketSettings;
     }
 
 }
