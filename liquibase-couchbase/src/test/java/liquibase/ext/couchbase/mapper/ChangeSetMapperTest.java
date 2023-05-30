f86214a0870deb2193c06bb26c78b798d543c64f
Dmitry Shanko <Dmitry.Shanko@tesco.com>
2023-05-30 12:56:13 +0800

    COS-191: improve test coverage on Utils

--- liquibase-couchbase/src/test/java/liquibase/ext/couchbase/mapper/ChangeSetMapperTest.java
+++ liquibase-couchbase/src/test/java/liquibase/ext/couchbase/mapper/ChangeSetMapperTest.java
@@ -1,9 +1,7 @@
 package liquibase.ext.couchbase.mapper;
 
-import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -14,32 +12,36 @@ import liquibase.changelog.ChangeSet;
 import liquibase.changelog.RanChangeSet;
 import liquibase.ext.couchbase.changelog.Context;
 import liquibase.ext.couchbase.changelog.CouchbaseChangeLog;
-import org.junit.Ignore;
+import liquibase.util.LiquibaseUtil;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 public class ChangeSetMapperTest {
 
     @Test
     void Should_map_to_ran_change_set() {
-        CouchbaseChangeLog changeLog = new CouchbaseChangeLog();
-        changeLog.setFileName("fileName");
-        changeLog.setId("id");
-        changeLog.setAuthor("author");
-        changeLog.setCheckSum("7:2cdf9876e74347162401315d34b83746");
         Date date = new Date(2023, Calendar.MAY, 25, 12, 1, 2);
-        changeLog.setDateExecuted("2023.05.25 12:01:02");
-        changeLog.setTag("tag");
-        changeLog.setExecType(ChangeSet.ExecType.EXECUTED);
-        changeLog.setDescription("description");
-        changeLog.setComments("comments");
-        changeLog.setDeploymentId("deploymentId");
-        changeLog.setLabels(Stream.of("label1", "label2", "label3").collect(Collectors.toSet()));
+        String dateStr = "2023.05.25 12:01:02";
         Context context = new Context(new ContextExpression("context"));
-        changeLog.setContext(context);
-        changeLog.setOrderExecuted(999);
-        changeLog.setLiquibaseVersion("version");
+
+        CouchbaseChangeLog changeLog = Mockito.mock(CouchbaseChangeLog.class);
+        when(changeLog.getFileName()).thenReturn("fileName");
+        when(changeLog.getId()).thenReturn("id");
+        when(changeLog.getAuthor()).thenReturn("author");
+        when(changeLog.getCheckSum()).thenReturn("7:2cdf9876e74347162401315d34b83746");
+        when(changeLog.getDateExecuted()).thenReturn(dateStr);
+        when(changeLog.getTag()).thenReturn("tag");
+        when(changeLog.getExecType()).thenReturn(ChangeSet.ExecType.EXECUTED);
+        when(changeLog.getDescription()).thenReturn("description");
+        when(changeLog.getComments()).thenReturn("comments");
+        when(changeLog.getDeploymentId()).thenReturn("deploymentId");
+        when(changeLog.getLabels()).thenReturn(Stream.of("label1", "label2", "label3").collect(Collectors.toSet()));
+        when(changeLog.getContext()).thenReturn(context);
+        when(changeLog.getOrderExecuted()).thenReturn(999);
+        when(changeLog.getLiquibaseVersion()).thenReturn("version");
 
         RanChangeSet expected = new RanChangeSet(
                 changeLog.getFileName(),
@@ -58,7 +60,47 @@ public class ChangeSetMapperTest {
         expected.setOrderExecuted(changeLog.getOrderExecuted());
         expected.setLiquibaseVersion(changeLog.getLiquibaseVersion());
 
-        RanChangeSet result =  ChangeSetMapper.mapToRanChangeSet(changeLog);
+        RanChangeSet result = ChangeSetMapper.mapToRanChangeSet(changeLog);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void Should_map_to_couchbase_change_log() {
+        String liquibaseVersion = LiquibaseUtil.getBuildVersion();
+        String dateStr = "2023.05.25 12:01:02";
+
+        CheckSum checkSum = CheckSum.parse("7:2cdf9876e74347162401315d34b83746");
+
+        Labels labels = Mockito.mock(Labels.class);
+        when(labels.getLabels()).thenReturn(Stream.of("label1", "label2", "label3").collect(Collectors.toSet()));
+
+        ContextExpression contextExpression = Mockito.mock(ContextExpression.class);
+
+        ChangeSet changeSet = Mockito.mock(ChangeSet.class);
+        when(changeSet.getFilePath()).thenReturn("filePath");
+        when(changeSet.getId()).thenReturn("id");
+        when(changeSet.getAuthor()).thenReturn("author");
+        when(changeSet.generateCheckSum()).thenReturn(checkSum);
+        when(changeSet.getDescription()).thenReturn("description");
+        when(changeSet.getComments()).thenReturn("comments");
+        when(changeSet.getLabels()).thenReturn(labels);
+        when(changeSet.getContextFilter()).thenReturn(contextExpression);
+
+        CouchbaseChangeLog expected = CouchbaseChangeLog.builder()
+                .fileName(changeSet.getFilePath())
+                .id(changeSet.getId())
+                .author(changeSet.getAuthor())
+                .checkSum(checkSum.toString())
+                .dateExecuted(dateStr)
+                .description(changeSet.getDescription())
+                .comments(changeSet.getComments())
+                .labels(changeSet.getLabels().getLabels())
+                .context(new Context(changeSet.getContextFilter()))
+                .liquibaseVersion(liquibaseVersion)
+                .build();
+
+        CouchbaseChangeLog result = ChangeSetMapper.mapToCouchbaseChangeLog(changeSet);
+        result.setDateExecuted(dateStr);
         assertThat(result).isEqualTo(expected);
     }
 }
