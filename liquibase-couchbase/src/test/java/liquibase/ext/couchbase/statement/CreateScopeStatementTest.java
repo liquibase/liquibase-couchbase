6e5f0a961f0399bb07abb89705c0471d075681f8
Dmitry Shanko <Dmitry.Shanko@tesco.com>
2023-05-31 14:20:32 +0800

    COS-191: more tests

---
+++ liquibase-couchbase/src/test/java/liquibase/ext/couchbase/statement/CreateScopeStatementTest.java
@@ -0,0 +1,30 @@
+package liquibase.ext.couchbase.statement;
+
+import liquibase.ext.couchbase.operator.BucketOperator;
+import liquibase.ext.couchbase.operator.ClusterOperator;
+import org.junit.jupiter.api.Test;
+
+import static common.constants.TestConstants.TEST_BUCKET;
+import static common.constants.TestConstants.TEST_KEYSPACE;
+import static common.constants.TestConstants.TEST_SCOPE;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class CreateScopeStatementTest {
+
+    private final ClusterOperator clusterOperator = mock(ClusterOperator.class);
+    private final BucketOperator bucketOperator = mock(BucketOperator.class);
+
+    @Test
+    void Should_call_createScope() {
+        CreateScopeStatement statement = new CreateScopeStatement(TEST_SCOPE, TEST_BUCKET);
+
+        when(clusterOperator.getBucketOperator(TEST_KEYSPACE.getBucket())).thenReturn(bucketOperator);
+
+        statement.execute(clusterOperator);
+
+        verify(bucketOperator).createScope(TEST_SCOPE);
+    }
+
+}
